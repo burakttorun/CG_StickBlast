1eed352201ea3d83791d7f9fb8fa15a94882a515
burakttorun <burakttorun@hotmail.com>
2024-10-24 02:45:29 +0300

    MapCreationManager refactored.

--- Assets/_ThePrototype/_Scripts/Manager/MapCreationManager.cs
+++ Assets/_ThePrototype/_Scripts/Manager/MapCreationManager.cs
@@ -1,18 +1,21 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ThePrototype.Scripts.Managers
 {
     public class MapCreationManager : MonoBehaviour
     {
-        [Header("References")] public GameObject edgePrefab;
+        [Header("References")]
+        public GameObject edgePrefab;
         public GameObject cellPrefab;
-        public GameObject pointPrefab;
+        public GameObject nodePrefab;
 
-        [Header("Settings")] [SerializeField] private float cellSize = 1.0f;
+        [Header("Settings")] 
+        [SerializeField] private float cellSize = 1.0f;
         [SerializeField] private float edgeWidth = 0.2f;
         [SerializeField] private float edgeHeight = 0.2f;
-        [SerializeField] private float pointSize = 0.2f;
+        [SerializeField] private float nodeSize = 0.2f;
 
         private int _columns;
         private int _rows;
@@ -25,87 +28,63 @@ namespace ThePrototype.Scripts.Managers
             _gridDataManager = GameManager.Instance.GridDataManager;
             _columns = GameManager.Instance.Columns;
             _rows = GameManager.Instance.Rows;
-            _startX = (_columns - 1) * (cellSize + edgeWidth) / 2;
-            _startY = (_rows - 1) * (cellSize + edgeHeight) / 2;
+
             CreateGrid();
         }
 
         private void CreateGrid()
         {
-            for (int r = 0; r < _rows; r++)
+            float startX = (_columns - 1) * (cellSize + edgeWidth) / 2;
+            float startY = (_rows - 1) * (cellSize + edgeHeight) / 2;
+
+            for (int row = 0; row <= _rows; row++)
             {
-                for (int c = 0; c < _columns; c++)
+                for (int col = 0; col <= _columns; col++)
                 {
-                    Vector3 cellPosition = new Vector3(c * (cellSize + edgeWidth) - _startX,
-                        -r * (cellSize + edgeHeight) + _startY, 0);
-                    var cell = Instantiate(cellPrefab, cellPosition, Quaternion.identity);
-                    cell.transform.localScale = new Vector3(cellSize, cellSize, 1);
-
-                    cell.name = $"cell_{c}_{r}";
-                    _gridDataManager.SetCell(r, c, cell.GetComponent<CellManager>());
-                }
-            }
-
-            CreateHorizontalEdges();
-            CreateVerticleEdges();
-            CreateCirclePoints();
-        }
-
-        private void CreateHorizontalEdges()
-        {
-            for (int r = 0; r <= _rows; r++)
-            {
-                for (int c = 0; c < _columns; c++)
-                {
-                    Vector3 cellPosition = new Vector3(c * (cellSize + edgeWidth) - _startX,
-                        -r * (cellSize + edgeHeight) + _startY, 0);
-                    Vector3 edgePosition = cellPosition + new Vector3(0, (cellSize / 2) + (edgeHeight / 2), 0);
-                    var topEdge = Instantiate(edgePrefab, edgePosition, Quaternion.identity);
-                    topEdge.transform.localScale = new Vector3(cellSize, edgeHeight, 1);
-                    topEdge.name = $"edge_{c}_{r}_horizontal";
-                    _gridDataManager.SetHorizontalEdge(r, c, false);
-                }
-            }
-        }
-
-        private void CreateVerticleEdges()
-        {
-            for (int r = 0; r < _rows; r++)
-            {
-                for (int c = 0; c <= _columns; c++)
-                {
-                    Vector3 cellPosition = new Vector3(c * (cellSize + edgeWidth) - _startX,
-                        -r * (cellSize + edgeHeight) + _startY, 0);
-                    Vector3 edgePosition = cellPosition - new Vector3((cellSize / 2) + (edgeWidth / 2), 0, 0);
-                    var leftEdge = Instantiate(edgePrefab, edgePosition, Quaternion.Euler(0, 0, 90));
-                    leftEdge.transform.localScale = new Vector3(cellSize, edgeHeight, 1);
-                    leftEdge.name = $"edge_{c}_{r}_verticle";
-                    _gridDataManager.SetVerticalEdge(r, c, false);
-                }
-            }
-        }
-
-
-        private void CreateCirclePoints()
-        {
-            for (int r = 0; r <= _rows; r++)
-            {
-                for (int c = 0; c <= _columns; c++)
-                {
-                    Vector3 cellPosition = new Vector3(c * (cellSize + edgeHeight) - _startX,
-                        -r * (cellSize + edgeHeight) + _startY, 0);
-                    Vector3 circlePosition = cellPosition + new Vector3(-1 * ((cellSize / 2) + (edgeWidth / 2)),
+                    Vector3 cellPosition = new Vector3(col * (cellSize + edgeWidth) - startX,
+                        -row * (cellSize + edgeHeight) + startY, 0);
+                    Vector3 nodePosition = cellPosition + new Vector3(-1 * ((cellSize / 2) + (edgeWidth / 2)),
                         (cellSize / 2) + (edgeHeight / 2), 0);
-                    CreateCircleAtPosition(circlePosition, c, r);
+                    CreateNodeAtPosition(nodePosition, col, row);
+
+                    if (col < _columns)
+                    {
+                        Vector3 horizontalEdgePosition =
+                            cellPosition + new Vector3(0, (cellSize / 2) + (edgeHeight / 2), 0);
+                        CreateEdgeOnPosition(horizontalEdgePosition, col, row, 0, "horizontal");
+                        _gridDataManager.SetHorizontalEdge(row, col, false);
+                    }
+
+                    if (row < _rows)
+                    {
+                        Vector3 edgePosition = cellPosition - new Vector3((cellSize / 2) + (edgeWidth / 2), 0, 0);
+                        CreateEdgeOnPosition(edgePosition, col, row, 90, "vertical");
+                        _gridDataManager.SetVerticalEdge(row, col, false);
+                    }
+
+                    if (col < _columns && row < _rows)
+                    {
+                        var cell = Instantiate(cellPrefab, cellPosition, Quaternion.identity);
+                        cell.transform.localScale = new Vector3(cellSize, cellSize, 1);
+
+                        cell.name = $"cell_{row}_{col}";
+                        _gridDataManager.SetCell(row, col, cell.GetComponent<CellManager>());
+                    }
                 }
             }
         }
-
-        private void CreateCircleAtPosition(Vector3 position, int circleX, int circleY)
+        private void CreateEdgeOnPosition(Vector3 edgePosition, int col, int row, int angle, string direction)
         {
-            var circle = Instantiate(pointPrefab, position, Quaternion.identity);
-            circle.transform.localScale = new Vector3(pointSize, pointSize, 1);
-            circle.name = $"circle_{circleX}_{circleY}";
+            var leftEdge = Instantiate(edgePrefab, edgePosition, Quaternion.Euler(0, 0, angle));
+            leftEdge.transform.localScale = new Vector3(cellSize, edgeHeight, 1);
+            leftEdge.name = $"edge_{col}_{row}_{direction}";
+        }
+
+        private void CreateNodeAtPosition(Vector3 position, int col, int row)
+        {
+            var node = Instantiate(nodePrefab, position, Quaternion.identity);
+            node.transform.localScale = new Vector3(nodeSize, nodeSize, 1);
+            node.name = $"node_{col}_{row}";
         }
     }
 }
